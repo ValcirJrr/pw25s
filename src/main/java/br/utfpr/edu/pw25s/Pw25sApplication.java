b9efcc399b443194c9c5f529ffb3a384d60b452a
valcir <valcir@viasoft.com.br>
2021-10-06 20:22:47 -0300

    Initial commit

---
+++ src/main/java/br/utfpr/edu/pw25s/Pw25sApplication.java
@@ -0,0 +1,13 @@
+package br.utfpr.edu.pw25s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw25sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pw25sApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/utfpr/edu/pw25s/Pw25sApplicationTests.java
@@ -0,0 +1,13 @@
+package br.utfpr.edu.pw25s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw25sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
